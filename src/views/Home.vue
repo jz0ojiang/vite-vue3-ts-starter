5bc4ef5443af0fc04ffea45edbf501ff369d409c
0ojixueseno0 <jz0ojiang@qq.com>
2022-07-24 20:59:34 +0800

    :tada: template commit

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>views/Home.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite + Router + SCSS starter
+  </p>
+  <p> custom stater by jz0ojiang </p>
+  <div class="card">
+    <p>
+        Docs What u will need
+        <ul>
+            <li>
+                <a href="https://router.vuejs.org/zh/guide/essentials/history-mode.html">use history-mode</a>
+            </li>
+        </ul>
+    </p>
+  </div>
+  <p>
+    Install
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
